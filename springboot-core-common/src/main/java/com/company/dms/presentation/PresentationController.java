f940a36b8bfb546f18d799c7436cb7080b243d9f
황진욱 <40532902+RoaZium@users.noreply.github.com>
2024-07-16 23:24:45 +0900

    feat: Tag 추가

--- springboot-core-common/src/main/java/com/company/dms/presentation/PresentationController.java
+++ springboot-core-common/src/main/java/com/company/dms/presentation/PresentationController.java
@@ -1,5 +1,6 @@
 package com.company.dms.presentation;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/presentations")
+@Tag(name = "presentations", description = "Presentations Management APIs")
 public class PresentationController {
 
     @Autowired
