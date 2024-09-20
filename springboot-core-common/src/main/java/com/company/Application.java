e7495e79e446b362dafa25b4e0129c3f10548fd6
황진욱 <RoaZium@gmail.com>
2024-09-20 11:28:44 +0900

    feat: Launcher4j Library 추가

--- springboot-core-common/src/main/java/com/company/Application.java
+++ springboot-core-common/src/main/java/com/company/Application.java
@@ -5,13 +5,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 @EnableAutoConfiguration
 @SpringBootApplication
 @RestController
 public class Application {
 
-    public static
-    void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    private static ServerSocket serverSocket;
+
+    public static void main(String[] args) {
+        if (isPortInUse(38080)) {
+            System.out.println("Application is already running.");
+            System.exit(1);
+        } else {
+            SpringApplication.run(Application.class, args);
+            System.out.println("Application started successfully.");
+        }
+    }
+
+    private static boolean isPortInUse(int port) {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            return false;
+        } catch (IOException e) {
+            return true;
+        }
     }
 }
