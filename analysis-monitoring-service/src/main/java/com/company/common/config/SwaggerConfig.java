4540a3295d8570d94fe28d37154895d2fe0b72f5
황진욱 <40532902+RoaZium@users.noreply.github.com>
2024-11-23 12:41:41 +0900

    2411231241

---
+++ analysis-monitoring-service/src/main/java/com/company/common/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.company.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${spring.application.name:AMS}")
+    private String applicationName;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(applicationName + " API Documentation")
+                        .description("Analysis Monitoring Service API 문서")
+                        .version("v1.0.0")
+                        .contact(new Contact()
+                                .name("개발팀")
+                                .email("dev@company.com")
+                                .url("https://company.com"))
+                        .license(new License()
+                                .name("Apache License Version 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0")));
+    }
+}
